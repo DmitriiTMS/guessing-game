6cd543feb527aa2dd006b639c10f31836d7be5c7
DmitriiTMS <dmitrii12_88@mail.ru>
2024-08-28 13:43:57 +0300

    feat: finish

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.candidateName;
+      }
+    
+      setRange(min, max) {
+         this.min = min;
+        this.max = max;
+      }
+    
+      guess() {
+        return (this.candidateName = Math.ceil((this.min + this.max) / 2));
+      }
+    
+      lower() {
+        this.max = this.candidateName;
+      }
+    
+      greater() {
+        this.min = this.candidateName;
+      }
 }
 
 module.exports = GuessingGame;
